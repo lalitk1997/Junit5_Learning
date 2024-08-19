4cc3d9a233e2b3d555b49244108f58ef390b2659
lalitk1997 <lalit.k1997@gmail.com>
2024-08-20 04:09:54 +0530

    Initialize Project

---
+++ HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,18 @@
+package com.healthycoderapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void isDietRecommended() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findCoderWithWorstBMI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBMIScores() {
+    }
+}
