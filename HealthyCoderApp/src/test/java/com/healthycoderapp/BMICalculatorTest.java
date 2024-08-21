2a724ee21104f5c8ddf22b7f3683c9dd43027ff5
lalitk1997 <lalit.k1997@gmail.com>
2024-08-22 03:28:49 +0530

    Parameterized-Test Repeated-Test Test-Performance Assumptions Code-Organisation-Inner-Class

--- HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,16 +1,24 @@
 package com.healthycoderapp;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import jdk.jfr.Name;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
+
+    String simulateEnvironment = "dev";
 
     @BeforeAll
     public static void beforeAllTestCases(){
@@ -22,26 +30,89 @@ class BMICalculatorTest {
         System.out.println("After All TestCases");
     }
 
-    @Test
-    void shouldReturnTrueWhenDietRecommended () {
-        // Given
-        double weight = 89.0;
-        double height = 1.72;
-        // When
-        boolean recommended = BMICalculator.isDietRecommended(weight, height);
-        // Then
-        assertTrue(recommended);
+    @Nested
+    class IsDietRecommended{
+
+        @Test
+        void shouldReturnTrueWhenDietRecommended () {
+            // Given
+            Assumptions.assumeTrue(BMICalculatorTest.this.simulateEnvironment.equals("prod"));
+            double weight = 89.0;
+            double height = 1.72;
+            // When
+            boolean recommended = BMICalculator.isDietRecommended(weight, height);
+            // Then
+            assertTrue(recommended);
+        }
+
+        @Test
+        void shouldReturnFalseWhenNotDietRecommended() {
+            // Given
+            double weight = 50.0;
+            double height = 1.92;
+
+            // When
+            boolean result = BMICalculator.isDietRecommended(weight, height);
+            // Then
+            assertFalse(result);
+        }
+
+        @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
+        void shouldReturnFalseWhenNotDietRecommendedRepeat() {
+            // Given
+            double weight = 50.0;
+            double height = 1.92;
+            // When
+            boolean result = BMICalculator.isDietRecommended(weight, height);
+            // Then
+            assertFalse(result);
+        }
+
+        // Parameterized
+        @ParameterizedTest
+        @ValueSource(doubles = {70.0, 89.9, 90.2, 99.9})
+        void shouldReturnFalseWhenNotRecommendedParameter(double coderWeight) {
+            // Given
+            double weight = coderWeight;
+            double height = 1.72;
+            // When
+            boolean result = BMICalculator.isDietRecommended(weight, height);
+            // Then
+            assertFalse(result);
+
+        }
+
     }
 
-    @Test
-    void shouldReturnFalseWhenNotDietRecommended() {
+
+
+
+
+
+
+
+    @ParameterizedTest(name = "weight{0}, height{1}")
+    @CsvSource({
+            "90.0, 1.72",
+            "80.0, 1.42",
+            "99.8, 1.12"
+    })
+    void shouldReturnTrueWhenRecommendedParameter(double weight, double height) {
         // Given
-        double weight = 50.0;
-        double height = 1.92;
         // When
         boolean result = BMICalculator.isDietRecommended(weight, height);
         // Then
-        assertFalse(result);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest(name = "weight{0}, height{1}")
+    @CsvFileSource(resources = "/diet-recommended-input-data.csv", numLinesToSkip = 1)
+    void shouldReturnTrueWhenRecommendedParameterFile(double weight, double height) {
+        // Given
+        // When
+        boolean result = BMICalculator.isDietRecommended(weight, height);
+        // Then
+        assertTrue(result);
     }
 
     @Test
@@ -82,6 +153,7 @@ class BMICalculatorTest {
     }
 
     @Test
+    @DisabledOnOs(OS.MAC)
     void shouldReturnCorrectBMIScore(){
         // Given
         List<Coder> coders = new ArrayList<>();
@@ -93,6 +165,20 @@ class BMICalculatorTest {
         double[] bmiScores = BMICalculator.getBMIScores(coders);
         // Then
         assertArrayEquals(bmiScores, expectedScore, 0.04);
+    }
+
+    @Test
+    @DisplayName("Customized Name")
+    @Disabled
+    void shouldReturnCodersWithWorstBMIInMillisecond(){
+        // Given
+        List<Coder> coders = new ArrayList<>();
+        for(int i=0; i<10000; i++)
+                coders.add(new Coder(1.0 + i, 10.0 + i));
+        // When
+        Executable executable = () -> {BMICalculator.findCoderWithWorstBMI(coders);};
+        // Then
+        assertTimeout(Duration.ofMillis(3), executable);
     }
 
 }
